94f5951758a4585404d5d4f931fb1c8785ba64a0
Rachel Kroetch <rekroetch@ucdavis.edu>
2020-12-18 10:53:53 -0800

    delete route filters but does not persist

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,35 @@
-// cant export so can i not require...?
+
 const noteList = require("../db/db.json");
 console.log(noteList)
 const fs = require('fs')
+const uuid = require('uuid')
 
-// const noteList = []
 
 module.exports = function(app) {
   
   app.get("/api/notes", function(req, res) {
-    res.json(noteList);
-     // read file instead of above ^? seems like both work
-    //  fs.readFile('../db/db.json', 'utf8', (err, data) => {
-    //   //  console.log(data)
-    //   if (err) throw err;
-    // })
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(noteList);
+    })
   });
 
 
   app.post("/api/notes", function(req, res) {
-    // no idea how to handle the id thing yet
     
-    
-
     const newNote = {
-      // id: id,
+      id: uuid.v4(),
       title: req.body.title,
       text: req.body.text,
     }
 
     noteList.push(newNote)
 
-    // MAYBE TRY FS.APPENDFILE?
     fs.writeFile('./db/db.json', JSON.stringify(noteList), (err) => {
       if (err) throw err;
-      // id++
+    
     })
-    // does not work to add to db.json
-    // noteList.push(newNote);
+
     res.json(true);
     // does this return the entire noteList?
     res.json(noteList)
@@ -45,34 +38,21 @@ module.exports = function(app) {
   
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
-  // app.post("/api/clear", function(req, res) {
-  //   // Empty out the arrays of data
-  //   tableData.length = 0;
-  //   waitListData.length = 0;
+  app.delete("/api/notes/:id", (req, res) => {
 
-  //   res.json({ ok: true });
-  // });
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const updatedNoteList = noteList.filter(note => note.id !== req.params.id)
+      res.json(updatedNoteList)
 
-  // Displays a single character, or returns false
-  app.delete("/api/notes/:id", function(req, res) {
+      fs.writeFile('./db/db.json', JSON.stringify(updatedNoteList), (err) => {
+        if (err) throw err;
+      })
+    })
+    
     // from youtube
-    res.json(noteList.filter(note => note.id === parseInt(req.params.id)))
+    // res.json(noteList.filter(note => note.id === parseInt(req.params.id)))
 
-    // // from class
-    // var chosen = req.params.id;
-
-    // console.log(chosen);
-
-    // for (var i = 0; i < characters.length; i++) {
-    //   if (chosen === characters[i].routeName) {
-    //     return res.json(characters[i]);
-    //   }
-    // }
-
-    // return res.json(false);
   });
 };
